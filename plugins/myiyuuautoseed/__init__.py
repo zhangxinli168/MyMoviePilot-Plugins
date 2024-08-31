87173b5cfb2000dfc4421ae27b66a5fa0621d0aa
zhangxinli168 <289891807@qq.com>
2024-09-01 00:01:09 +0800

    Update __init__.py

--- plugins/myiyuuautoseed/__init__.py
+++ plugins/myiyuuautoseed/__init__.py
@@ -668,6 +668,7 @@ class MyIYUUAutoSeed(_PluginBase):
             # 下载器
             downloader_obj = self.__get_downloader("transmission")
             downloader="transmission"
+            
             # 获取下载器中的种子状态
             torrents, _ = downloader_obj.get_torrents(ids=recheck_torrents)
             if torrents:
