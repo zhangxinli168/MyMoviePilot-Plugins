9f0c1f91b7032af83e614706944d7043ea8b0fda
zhangxinli168 <289891807@qq.com>
2024-08-31 19:49:50 +0800

    1
    1

--- plugins/myiyuuautoseed/__init__.py
+++ plugins/myiyuuautoseed/__init__.py
@@ -821,6 +821,7 @@ class IYUUAutoSeed(_PluginBase):
         """
         添加下载任务
         """
+        downloader="transmission"
         if downloader == "qbittorrent":
             # 生成随机Tag
             tag = StringUtils.generate_random_str(10)
@@ -970,7 +971,8 @@ class IYUUAutoSeed(_PluginBase):
                 # TR会自动校验
                 if downloader == "qbittorrent":
                     # 开始校验种子
-                    downloader_obj.recheck_torrents(ids=[download_id])
+                    #downloader_obj.recheck_torrents(ids=[download_id])
+                    pass
             # 下载成功
             logger.info(f"成功添加辅种下载，站点：{site_info.get('name')}，种子链接：{torrent_url}")
             # 成功也加入缓存，有一些改了路径校验不通过的，手动删除后，下一次又会辅上
